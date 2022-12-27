9a403e9dedbf8b0dbb128892bceb7691448f414a
Juan Arede <85207414+juanarede@users.noreply.github.com>
2022-12-26 22:34:07 -0300

    button

--- src/components/Proyecto/proyecto.jsx
+++ src/components/Proyecto/proyecto.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./proyecto.css";
 import { CircularProgressbar } from "react-circular-progressbar";
 import GradientSVG from "./gradientSVG";
+import { Link } from "react-router-dom";
 
 function proyecto() {
   const idCSS = "hello";
@@ -26,10 +27,7 @@ function proyecto() {
                       </p>
                     </div>
 
-
-
-
-                     <div
+                    <div
                       style={{ paddingBottom: "18rem" }}
                       class="card card-shadown text-white"
                     >
@@ -59,43 +57,45 @@ function proyecto() {
                               </a>
                             </li>
                           </ul>
-                    
-                   
-                    
-                   
-
-
-
                         </div>
                       </div>
-                    </div> 
+                    </div>
 
                     <div
-                    style={{ marginTop:"0.4rem"}}
-                    class="card  text-white"
-                  >
-                    <div class="card-body">
-                      <h5 class="card-title text-light d-flex justify-content-center">
-                        Clasificación
-                      </h5>
-                      <div className="container">
-                        <div className="row">
-                          <div className="col-6">
-                             <h6 className="text-light">Nombre:</h6>
-                             <h6 className="text-light">Latitud:</h6>
-                             <h6 className="text-light">Longitud:</h6>
-                          </div>
-                          <div className="col-6">
-
+                      style={{ marginTop: "0.4rem" }}
+                      class="card  text-white mb-4"
+                    >
+                      <div class="card-body">
+                        <h5 class="card-title text-light d-flex justify-content-center">
+                          Clasificación
+                        </h5>
+                        <div className="container">
+                          <div className="row">
+                            <div className="col-6">
+                              <h6
+                                style={{ marginLeft: "1rem" }}
+                                className="text-light"
+                              >
+                                Nombre:
+                              </h6>
+                              <h6
+                                style={{ marginLeft: "1rem" }}
+                                className="text-light"
+                              >
+                                Latitud:
+                              </h6>
+                              <h6
+                                style={{ marginLeft: "1rem" }}
+                                className="text-light"
+                              >
+                                Longitud:
+                              </h6>
+                            </div>
+                            <div className="col-6"></div>
                           </div>
                         </div>
                       </div>
-
-                      
-                      
                     </div>
-                  </div>
-
                   </div>
                   <div class="col-sm-8 col-md-6">
                     <div className="container">
@@ -120,55 +120,63 @@ function proyecto() {
                             />
                           </div>
                         </div>
-            
                       </div>
                       <div className="container">
-                      <div className="row mt-3">
-                        <div className="col-1"></div>
-                        <div className="col-12 col-md-5 col-lg-5 mt-3">
-                          <div style={{ height: "220px", width: "200px" }}>
-                            <GradientSVG />
-                            <CircularProgressbar
-                              strokeWidth={8}
-                              value={percentage}
-                              text={percentage}
-                              styles={{
-                                path: {
-                                  stroke: `url(#${idCSS})`,
-                                  height: "100%",
-                                },
-                                trail: {
-                                  stroke: "#f2f2f2",
-                                },
-                              }}
-                            />
+                        <div className="row mt-3">
+                          <div className="col-1"></div>
+                          <div className="col-12 col-md-5 col-lg-5 mt-3">
+                            <div style={{ height: "220px", width: "200px" }}>
+                              <GradientSVG />
+                              <CircularProgressbar
+                                strokeWidth={8}
+                                value={percentage}
+                                text={percentage}
+                                styles={{
+                                  path: {
+                                    stroke: `url(#${idCSS})`,
+                                    height: "100%",
+                                  },
+                                  trail: {
+                                    stroke: "#f2f2f2",
+                                  },
+                                }}
+                              />
+                            </div>
                           </div>
-                        </div>
 
-                        <div className="col-8 col-md-5 col-lg-5 mt-3">
-                          <div style={{ height: "220px", width: "200px" }}>
-                            <GradientSVG />
-                            <CircularProgressbar
-                              strokeWidth={8}
-                              value={percentage}
-                              text={percentage}
-                              styles={{
-                                path: {
-                                  stroke: `url(#${idCSS})`,
-                                  height: "100%",
-                                },
-                                trail: {
-                                  stroke: "#f2f2f2",
-                                },
-                              }}
-                            />
+                          <div className="col-8 col-md-5 col-lg-5 mt-3">
+                            <div style={{ height: "220px", width: "200px" }}>
+                              <GradientSVG />
+                              <CircularProgressbar
+                                strokeWidth={8}
+                                value={percentage}
+                                text={percentage}
+                                styles={{
+                                  path: {
+                                    stroke: `url(#${idCSS})`,
+                                    height: "100%",
+                                  },
+                                  trail: {
+                                    stroke: "#f2f2f2",
+                                  },
+                                }}
+                              />
+                            </div>
                           </div>
                         </div>
-                        
-                      </div>
                       </div>
                     </div>
-                    <h6>dsadsadsadsa</h6>
+                    <div className="d-flex justify-content-center button-map">
+                      <Link to="/mapa">
+                        <a href="" title="">
+                          <button class="myButton">
+                         
+                          <i style={{marginRight:"0.3rem"}} class="fa-solid fa-location-dot "></i>
+                            Ver Mapa
+                          </button>
+                        </a>
+                      </Link>
+                    </div>
                   </div>
                 </div>
               </div>
