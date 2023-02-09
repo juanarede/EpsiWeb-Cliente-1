9ecd16dbae1456d37cdf5ca43ed1f2c54fd727be
loadEdar <eduardo.arede@gmail.com>
2023-02-09 10:10:10 -0300

    cambios en footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
     <div style={{paddingTop:"2rem",paddingBottom:"1rem"}} className='bg-black d-flex justify-content-center align-items-center'>
-        <h6 className='text-gray-500' style={{color:"#fff"}}>Hecho por <a href='https://epsiweb.com/' style={{cursor:"pointer"}}><img style={{width:"6rem"}} src={LogoWithe} alt="logo epsiweb"/></a></h6>
+        <h6 className='text-gray-500' style={{color:"#fff"}}>M.A.L.M.V | 2023</h6>
         </div>
     </>
   )
