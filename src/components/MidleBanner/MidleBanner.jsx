0a3870ded41a4c21f504773c40fc5e24d6932ea2
loadEdar <eduardo.arede@gmail.com>
2023-02-14 19:17:18 -0300

    correccion de ortografia

--- src/components/MidleBanner/MidleBanner.jsx
+++ src/components/MidleBanner/MidleBanner.jsx
@@ -11,7 +11,7 @@ function MidleBanner() {
    <div className='row'>
       
    <div className=' col-12 p-3  text-center'> 
-  <h5 className='title-banner lead'> Al profesor Matt Zigler por envíanos sus diseños del droposcope el cual modificamos y realizamos planos y animaciones 3D, Agustin Landaburo quien nos ayudo con la correcta redacción de los textos de presentaron a los concursos. Carlos Ignacio Bertoli por todo su asesoramiento sobre microscopios y ayudada durante el proyecto sobre microorganismos. Alejandro Greco por su paciencia y ayuda en el correcto manejo de los punteros láser.</h5>
+  <h5 className='title-banner lead'> Al profesor Matt Zigler por envíanos sus diseños del droposcope el cual modificamos y realizamos planos y animaciones 3D, Agustin Landaburo quien nos ayudo con la correcta redacción de los textos de presentaron a los concursos. Carlos Ignacio Bertoli por todo su asesoramiento sobre microscopios y ayuda durante el proyecto sobre microorganismos. Alejandro Greco por su paciencia y ayuda en el correcto manejo de los punteros láser.</h5>
   </div>
    </div>
 
